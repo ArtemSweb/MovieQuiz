36de686a3c09006f50f6634f15f78c118a185f10
Solodovnikov Artem <arsolodovnikov@ozon.ru>
2024-11-14 23:18:16 +0300

    задача 5го спринта

---
+++ MovieQuiz/Presentation/AlertPresenter.swift
@@ -0,0 +1,29 @@
+import UIKit
+
+final class AlertPresenter {
+    private weak var delegate: UIViewController?
+
+    func setup(delegate: UIViewController) {
+        self.delegate = delegate
+    }
+    
+    func createAlert(_ model: AlertModel) -> UIAlertController {
+        let alert = UIAlertController(
+            title: model.title,
+            message: model.message,
+            preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: model.buttonText, style: .default) { _ in
+            model.completion?()
+        }
+        
+        alert.addAction(action)
+        return alert
+    }
+    
+    func showAlert(model: AlertModel) {
+        let alert = createAlert(model)
+        
+        delegate?.present(alert, animated: true, completion: nil)
+    }
+}
