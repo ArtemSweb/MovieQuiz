bf4e1e257deb7c3ed2a8e4c3e86efd464c32d0f8
Solodovnikov Artem <arsolodovnikov@ozon.ru>
2024-12-03 00:03:07 +0300

    Добавлены тесты на показ Алерта в конце квиза и на перезапуск игры по клику на кнопку в алерте

--- MovieQuiz/Presentation/AlertPresenter.swift
+++ MovieQuiz/Presentation/AlertPresenter.swift
@@ -12,11 +12,11 @@ final class AlertPresenter {
             title: model.title,
             message: model.message,
             preferredStyle: .alert)
+        alert.view.accessibilityIdentifier = "Game result"
         
         let action = UIAlertAction(title: model.buttonText, style: .default) { _ in
             model.completion?()
         }
-        
         alert.addAction(action)
         return alert
     }
--- MovieQuizUITests/MovieQuizUITests.swift
+++ MovieQuizUITests/MovieQuizUITests.swift
@@ -20,10 +20,85 @@ final class MovieQuizUITests: XCTestCase {
         app.terminate()
         app = nil
     }
-    
-    func testScreenCast() throws {
-        
-        app.buttons["Нет"].tap()
 
+    //тест что кнопка "Да" перелистывает вопрос
+    func testYesButton() {
+        sleep(4)
+        let firstPoster = app.images["Poster"]
+        let firstPosterData = firstPoster.screenshot().pngRepresentation
+        
+        app.buttons["Yes"].tap()
+        sleep(4)
+        
+        let secontPoster = app.images["Poster"]
+        let secontPosterData = secontPoster.screenshot().pngRepresentation
+        
+        XCTAssertNotEqual(firstPosterData, secontPosterData)
+    }
+    
+    //тест что кнопка "Нет" перелистывает вопрос
+    func testNoButton() {
+        sleep(4)
+        let firstPoster = app.images["Poster"]
+        let firstPosterData = firstPoster.screenshot().pngRepresentation
+        
+        app.buttons["No"].tap()
+        sleep(4)
+        
+        let secontPoster = app.images["Poster"]
+        let secontPosterData = secontPoster.screenshot().pngRepresentation
+        
+        XCTAssertNotEqual(firstPosterData, secontPosterData)
+    }
+    
+    //тест счетчика вопросов
+    func testIndexQuestionLable() {
+        let indexLabel = app.staticTexts["Index"]
+        
+        app.buttons["Yes"].tap()
+        sleep(2)
+        app.buttons["Yes"].tap()
+        sleep(2)
+        app.buttons["Yes"].tap()
+        sleep(2)
+        
+        XCTAssertEqual(indexLabel.label, "4/10")
+    }
+    
+    //тест появления алерта в конце квиза
+    func testShowAlert() {
+        //ждем загрузку первого вопроса
+        sleep(2)
+        
+        //10 раз жмем кнопку "Да"
+        for _ in 1...10 {
+            app.buttons["Yes"].tap()
+            sleep(3)
+        }
+        
+        sleep(2)
+        //ловим алерт
+        let alert = app.alerts["Game result"]
+        
+        //проверяем наличие алерта и верность текста заголовка и кнопки
+        XCTAssertTrue(alert.exists)
+        XCTAssertTrue(alert.label == "Этот раунд окончен!")
+        XCTAssertTrue(alert.buttons.firstMatch.label == "Cыграть еще раз")
+    }
+    
+    func testDismissAlert() {
+        testShowAlert()
+        
+        //ловим алерт
+        let alert = app.alerts["Game result"]
+        
+        alert.buttons.firstMatch.tap()
+        
+        sleep(2)
+        
+        let indexLabel = app.staticTexts["Index"]
+        
+        XCTAssertFalse(alert.exists)
+        XCTAssertEqual(indexLabel.label, "1/10")
     }
 }
